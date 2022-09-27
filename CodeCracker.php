0aa9a1b264d85db354ef4290fe14ec512890b2fe
Wojciech <wkonieczny.dev@gmail.com>
2022-09-27 17:16:00 +0200

    Formating CodeCracker.php and deleting unnessesery files.

---
+++ CodeCracker.php
@@ -0,0 +1,43 @@
+<?php
+
+declare (strict_types=1);
+
+namespace App;
+
+class CodeCracker 
+{   
+    public array $encodeSings = ["!", ")", '"', "(", "£", "*", "%", "&", ">", "<", "@", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"];
+    public array $decodeSings = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    public function decode(string $inputValue)
+    {
+       return $this->logic($inputValue, $this->encodeSings, $this->decodeSings);
+    }
+
+    public function encode(string $inputValue)
+    {
+       return $this->logic($inputValue, $this->decodeSings,$this->encodeSings);
+    }
+
+    public function logic(string $inputValue, array $codeKey, array $codedSings)
+    {
+        $inputValueArray= mb_str_split(strtolower($inputValue));
+
+        foreach ($inputValueArray as $letter) {
+            if (in_array($letter, $codeKey)) {
+                for ($i=0; $i < count($codeKey); $i++) { 
+                    if ($letter === $codeKey[$i]) {
+                        $outputMessage[] = $codedSings[$i]; 
+                    }
+                }
+            }else{
+                $outputMessage[] = $letter;
+            }
+        }
+        return htmlspecialchars(implode($outputMessage));
+    }
+}
+
+$message = new CodeCracker();
+echo $message->decode(')g!ld, j(!ad "> h>£ gdol>!o!" o!(!c>£');
+echo $message->encode('Zażółć, gęślą jaźń.');
